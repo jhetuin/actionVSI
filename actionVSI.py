64cc53052a7e238f94b3ec9c99f9f4a2762904d1
jhetuin <98955183+jhetuin@users.noreply.github.com>
2024-03-19 08:08:01 +0100

    Update actionVSI.py

--- actionVSI.py
+++ actionVSI.py
@@ -5,45 +5,35 @@ from ibm_cloud_sdk_core import ApiException
 
 #Authenticate user on IBM Cloud to do VPC VSI commands
 authenticator = IAMAuthenticator(os.environ.get('key'), url='https://iam.cloud.ibm.com')
-
 service = VpcV1(authenticator=authenticator)
 
-#  Listing VPCs
-print("List VPCs")
-try:
-    vpcs = service.list_vpcs().get_result()['vpcs']
-except ApiException as e:
-  print("List VPC failed with status code " + str(e.code) + ": " + e.message)
-for vpc in vpcs:
-    print(vpc['id'], "\t",  vpc['name'])
+#or
+#API_KEY = os.environ['api_key']
+#authenticator = IAMAuthenticator(API_KEY)
 
-#  Listing Subnets
-print("List Subnets")
-try:
-    subnets = service.list_subnets().get_result()['subnets']
-except ApiException as e:
-  print("List subnets failed with status code " + str(e.code) + ": " + e.message)
-for subnet in subnets:
-    print(subnet['id'], "\t",  subnet['name'])
-
-#  Listing Instances
-print("List Instances")
-try:
-    instances = service.list_instances().get_result()['instances']
-except ApiException as e:
-  print("List instances failed with status code " + str(e.code) + ": " + e.message)
-for instance in instances:
-    print(instance['id'], "\t",  instance['name'])
+#Set API endpoints
+#service.set_service_url('https://br-sao.iaas.cloud.ibm.com/v1')
+#service.set_service_url('https://eu-gb.iaas.cloud.ibm.com/v1')
+service.set_service_url('https://api.eu-gb.codeengine.cloud.ibm.com/v2')
 
 
-#  Updating Instance
-#print("Updated Instance")
-#try:
-#    newInstanceName = instanceName + "-1"
-#    instance = service.update_instance(
-#        id=instanceId,
-#        name=newInstanceName,
-#    ).get_result()
-#except ApiException as e:
-#    print("Update instance failed with status code " + str(e.code) + ": " + e.message)
-#print(instance['id'], "\t",  instance['name'])
+#Get the required action from environment variable
+VSIaction = os.environ['action']
+
+# List of instance ID to perform action
+instance_ids = []
+
+# Read list from environment variables (assume there will not be more that 5 VSIs)
+for VSI in range(1,5):
+    try:
+        instance_ids.append(os.environ['VSI_' + str(VSI)])
+    except:
+        break
+
+# Perform action on list
+for instance_id in instance_ids:
+    print(instance_id)
+    response = service.create_instance_action(
+        instance_id,
+        type = VSIaction,
+    )
